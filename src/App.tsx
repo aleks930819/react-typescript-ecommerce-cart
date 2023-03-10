e6a56a6fd95c5556711f181672506b1e5d9345e1
Aleksandar Atanasov <aleks930819@gmail.com>
2023-03-10 21:56:22 +0200

    added cart view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 
 function App() {
+  const isShown = true;
   return (
     <>
       <Header />
       <Products />
+      {isShown  && <Cart/> }
     </>
   );
 }
